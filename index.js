06c4a0d51c64dfd04c804640f01d79f1630680bc
Vlad Bars <vladshokoladkin@gmail.com>
2020-04-10 19:11:58 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+
+/*var dinasaurus = [
+    "поп",
+    "рот",
+    "пот",
+];
+console.log(dinasaurus.length);
+
+
+
+
+
+var dinasaurus = [];
+dinasaurus[0] = "fap";
+dinasaurus[1] = "tap";
+dinasaurus[2] = "lap";
+dinasaurus[0] = "bap";
+console.log(dinasaurus);
+
+var dinasaurusAndNmbers = [1, 2, 3, 5, "dinasaur", ["fap", "tap"]];
+
+console.log(dinasaurusAndNmbers[5][1]);*/
+
+
+
+var animals = [];
+
+animals.push("kot");
+animals.push("pes");
+animals.push("korova");
+
+console.log(animals);
+
+animals.pop();
+console.log(animals);
+
+
+
